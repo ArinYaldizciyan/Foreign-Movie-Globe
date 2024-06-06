1a2b0f50e4453e2978e9f9e0f2181312c90af51d
Arin Yaldizciyan <arin.yaldizciyan@gmail.com>
2024-06-06 03:18:00 -0400

    Added requirements.txt for python map generator

--- python/map.py
+++ python/map.py
@@ -1,42 +1,51 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from shapely.geometry import Polygon
+import math
+import csv
 
 # Load the world map data
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
+print(world['name'])
 # Ensure every country gets a unique shade of green
 def generate_shades_of_green(num_colors):
     colors = []
     for i in range(num_colors):
-        # Generate a shade of green in the format (R, G, B)
-        green_value = int(255 * (i / (num_colors - 1)))
+        # Generate a shade of green in the format (R, G, B) normalized to 0-1 range
+        green_value = i/255
         colors.append((0, green_value, 0))
     return colors
 
+
 # Get the number of countries
 num_countries = world.shape[0]
+print(num_countries)
 
 # Generate unique shades of green
-colors = generate_shades_of_green(min(255, num_countries))
+colors = generate_shades_of_green(num_countries)
 
 # Create a color map dictionary
 color_map = {}
 for i, country in enumerate(world['name']):
-    color_map[country] = colors[i % len(colors)]
+    color_map[country] = colors[i]
 
 # Plot the map
 fig, ax = plt.subplots(1, 1, figsize=(15, 10))
-world.plot(ax=ax, color=[mcolors.to_rgba(mcolors.rgb2hex(color_map[country])) for country in world['name']], edgecolor='black')
-
-# Create a legend
-handles = [plt.Line2D([0], [0], marker='o', color='w', label=country, markersize=10, markerfacecolor=mcolors.to_rgba(mcolors.rgb2hex(color_map[country]))) for country in world['name']]
-ax.legend(handles=handles, bbox_to_anchor=(1.05, 1), loc='upper left', fontsize='small')
+edge_color = '#00B100'
+world.plot(ax=ax, color=[mcolors.to_rgba(color_map[country]) for country in world['name']], edgecolor="none", antialiased=False)
 
 # Remove axis for better presentation
 ax.axis('off')
 
-# Save the figure
+# Save the figure without the legend
 plt.savefig('world_map_shades_of_green.png', bbox_inches='tight')
 plt.show()
+
+# Output the legend to a CSV file
+with open('country_green_shades.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Green Channel Value', 'Country Name'])
+    for country, color in color_map.items():
+        green_value = round(color[1] * 255)
+        writer.writerow([green_value, country])
